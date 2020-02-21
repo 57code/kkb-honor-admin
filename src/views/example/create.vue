7565a7631797d8e6b375b8ab60488226df580950
吴赛赛 <wusaisai335@163.com>
2020-02-21 19:04:51 +0800

    第二次提交

--- src/views/example/create.vue
+++ src/views/example/create.vue
@@ -1,5 +1,204 @@
 <template>
-  <article-detail :is-edit="false" />
+  <div class="app-container">
+    <el-row style="margin-bottom: 20px;">
+      <el-col
+        :span="1"
+        class="grid"
+      >
+        <el-button
+          type="danger"
+          icon="el-icon-circle-plus-outline"
+          size="mini"
+          round
+          @click="addFlag=true;dialogVisible = true "
+        >
+          新增
+        </el-button>
+      </el-col>
+    </el-row>
+    <el-table
+      v-loading="listLoading"
+      :data="tableData.slice((currentPage-1)*pagesize,currentPage*pagesize)"
+      border
+      fit
+      highlight-current-row
+      style="width: 100%"
+    >
+      <el-table-column
+        prop="index"
+        label="ID"
+        width="80px"
+        align="center"
+      >
+        <template slot-scope="scope">
+          <!-- <i class="el-icon-time"></i> -->
+          <span style="margin-left: 10px">{{ scope.row.id }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column
+        prop="name"
+        label="server"
+        width="200px"
+        align="center"
+      >
+        <template slot-scope="scope">
+          <!-- <i class="el-icon-time"></i> -->
+          <span style="margin-left: 10px">{{ scope.row.name }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column
+
+        label="status"
+        width="120px"
+        align="center"
+      >
+        <template slot-scope="scope">
+          <!-- <i class="el-icon-time"></i> -->
+          <span style="margin-left: 10px">{{ scope.row.status }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column
+        prop="val"
+        label="人气值"
+        width="100px"
+        align="center"
+      >
+        <template slot-scope="scope">
+          <!-- <i class="el-icon-time"></i> -->
+          <span style="margin-left: 10px">{{ scope.row.val }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column
+        label="server"
+        min-width="300px"
+        prop="server"
+      />
+
+      <el-table-column
+        label="操作"
+        width="300px"
+        align="center"
+      >
+        <template slot-scope="scope">
+          <el-button
+            type=""
+            size="small"
+            @click="handleClick(scope.row)"
+          >
+            查看
+          </el-button>
+          <el-button
+            size="mini"
+            @click="handleEdit(scope.$index, scope.row)"
+          >
+            编辑
+          </el-button>
+          <!-- <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button> -->
+          <el-button
+            plain
+            type=""
+            size="small"
+            @click.native.prevent="deleteRow(scope.$index, tableData)"
+          >
+            移除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <el-dialog
+      :title="addFlag?'新增服务器':'修改服务器'"
+      style="text-align:left !important"
+      :visible.sync="dialogVisible"
+      :before-close="handleClose"
+    >
+      <el-form
+        ref="form"
+        label-width="180px"
+      >
+        <el-form-item
+          label="ID"
+          style="width:280px"
+        >
+          <el-input
+            v-model="book.id"
+            placeholder="请输入ID值"
+          />
+        </el-form-item>
+        <el-form-item
+          label="name"
+          style="width:280px"
+        >
+          <el-input
+            v-model="book.name"
+            placeholder="请输入服务器名字"
+          />
+        </el-form-item>
+        <el-form-item
+          label="status"
+          style="width:280px"
+        >
+          <el-input
+            v-model="book.status"
+            placeholder="请输入服务器状态"
+          />
+        </el-form-item>
+        <el-form-item
+          label="server"
+          style="width:280px"
+        >
+          <el-input
+            v-model="book.server"
+            placeholder="请输入分区"
+          />
+        </el-form-item>
+      </el-form>
+      <span
+        slot="footer"
+        class="dialog-footer"
+      >
+        <el-button
+          type="success"
+          @click="saveBook()"
+        >提交</el-button>
+        <el-button
+          type="primary"
+          @click="dialogVisible = false"
+        >取消</el-button>
+      </span>
+    </el-dialog>
+    <el-dialog
+      title="提示"
+      style="text-align:left !important"
+      :visible.sync="dialog2Visible"
+      :before-close="handleClose"
+    >
+      <span>你确定要删除这本图书吗?</span>
+      <span
+        slot="footer"
+        class="dialog-footer"
+      >
+        <el-button @click="handleDel()">提交</el-button>
+        <el-button
+          type="primary"
+          @click="dialog2Visible = false"
+        >取消</el-button>
+      </span>
+    </el-dialog>
+    <el-pagination
+      :current-page="currentPage"
+      :page-sizes="[5, 10, 15, 20]"
+      :page-size="pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="tableData.length"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
+  </div>
 </template>
 
 <script lang="ts">
@@ -12,5 +211,180 @@ import ArticleDetail from './components/ArticleDetail.vue'
     ArticleDetail
   }
 })
-export default class extends Vue {}
+export default class extends Vue {
+  tableData = [{
+    id: 1,
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满',
+    val: 356
+  }, {
+    id: 2,
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满',
+    val: 356
+  }, {
+    id: 3,
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满',
+    val: 356
+
+  }, {
+    id: 4,
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满',
+    val: 356
+  }, {
+    id: 5,
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满',
+    val: 356
+  }, {
+    id: 6,
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满',
+    val: 356
+  }, {
+    id: 7,
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满',
+    val: 356
+  }, {
+    id: 8,
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满',
+    val: 356
+  }, {
+    id: 9,
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满',
+    val: 356
+  }, {
+    id: 10,
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满'
+  }, {
+    id: 12,
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满'
+  }, {
+    id: 13,
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满'
+  }, {
+    id: 14,
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满'
+  }, {
+    id: 15,
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满'
+  }, {
+    id: 16,
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满'
+  }, {
+    id: 17,
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满'
+  }, {
+    id: 18,
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满'
+  }, {
+    id: 19,
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满'
+  }, {
+    id: 20,
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满'
+  }]
+  book={}
+  dialogVisible= false
+  dialog2Visible= false
+  addFlag= true
+  pagesize=5
+  currentPage=1
+
+  handleSizeChange(size) {
+    this.pagesize = size
+  }
+  handleCurrentChange(currentPage) {
+    this.currentPage = currentPage
+  }
+
+  handleClick(row) {
+    this.$alert(row, 'hello', {
+      confirmButtonText: '确定'
+      // callback: action => {
+      // this.$message({
+      // type: 'info',
+      // message: `action: ${action}`
+      // })
+      // }
+    })
+  }
+  handleEdit(id, row) {
+    this.book = Object.assign({}, row)
+    this.dialogVisible = true
+    this.addFlag = false
+    // _id=id
+  }
+  deleteRow(index, rows) {
+    this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+      confirmButtonText: '确定',
+      cancelButtonText: '取消',
+      type: 'warning'
+    }).then(() => {
+      rows.splice(index, 1)
+      this.$message({
+        type: 'success',
+        message: '删除成功!'
+      })
+    }).catch(() => {
+      this.$message({
+        type: 'info',
+        message: '已取消删除'
+      })
+    })
+  }
+  async saveBook() {
+    try {
+      await this.tableData.push({
+        id: this.book.id,
+        name: this.book.name,
+        status: this.book.status,
+        server: this.book.server
+      })
+      this.dialogVisible = false
+      this.book = {}
+      this.$message({
+        message: '添加成功',
+        type: 'success'
+      })
+    // this.tableData
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
 </script>
--- src/views/example/list.vue
+++ src/views/example/list.vue
@@ -1,119 +1,31 @@
 <template>
   <div class="app-container">
-    <!-- <el-table v-loading="listLoading" :data="list"
-      border
-      fit
-      highlight-current-row
-      style="width: 100%"
-    >
-      <el-table-column
-        width="80"
-        align="center"
-        label="ID"
-
+    <el-row style="margin-bottom: 20px;">
+      <el-col
+        :span="1"
+        class="grid"
       >
-        <template slot-scope="scope">
-          <span>{{ scope.row.id }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column
-        width="180px"
-        align="center"
-        label="wechat"
-      >
-        <template slot-scope="scope">
-          <span>{{ scope.row.timestamp | parseTime }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column
-        width="180px"
-        align="center"
-        label="QQ"
-      >
-        <template slot-scope="scope">
-          <span>{{ scope.row.author }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column
-        width="105px"
-        label="人气值"
-      >
-        <template slot-scope="scope">
-          <svg-icon
-            v-for="n in +scope.row.importance"
-            :key="n"
-            name="star"
-            class="meta-item__icon"
-          />
-        </template>
-      </el-table-column>
-
-      <el-table-column
-        class-name="status-col"
-        label="Status"
-        width="110px"
-      >
-        <template slot-scope="{row}">
-          <el-tag :type="row.status | articleStatusFilter">
-            {{ row.status }}
-          </el-tag>
-        </template>
-      </el-table-column>
-
-      <el-table-column
-        min-width="200px"
-        label="备注"
-      >
-        <template slot-scope="{row}">
-          <router-link
-            :to="'/example/edit/'+row.id"
-            class="link-type"
-          >
-            <span>{{ row.title }}</span>
-          </router-link>
-        </template>
-      </el-table-column>
-
-      <el-table-column
-        align="center"
-        label="操作"
-        width="120px"
-      >
-        <template slot-scope="scope">
-          <router-link :to="'/example/edit/'+scope.row.id">
-            <el-button
-              type="primary"
-              size="small"
-              icon="el-icon-edit"
-            >
-            添加
-            </el-button>
-          </router-link>
-        </template>
-      </el-table-column>
-    </el-table> -->
-
-    <!-- <pagination
-      v-show="total>0"
-      :total="total"
-      :page.sync="listQuery.page"
-      :limit.sync="listQuery.limit"
-      @pagination="getList"
-    /> -->
-
+        <el-button
+          type="success"
+          icon="el-icon-circle-plus-outline"
+          size="mini"
+          round
+          @click="addFlag=true;dialogVisible = true "
+        >
+          新增
+        </el-button>
+      </el-col>
+    </el-row>
     <el-table
       v-loading="listLoading"
-      :data="tableData"
+      :data="tableData.slice((currentPage-1)*pagesize,currentPage*pagesize)"
       border
       fit
       highlight-current-row
       style="width: 100%"
     >
       <el-table-column
-        prop="id"
+        prop="index"
         label="ID"
         width="80px"
         align="center"
@@ -126,7 +38,7 @@
 
       <el-table-column
         prop="name"
-        label="servername"
+        label="server"
         width="200px"
         align="center"
       >
@@ -137,7 +49,7 @@
       </el-table-column>
 
       <el-table-column
-        prop="status"
+
         label="status"
         width="120px"
         align="center"
@@ -161,8 +73,9 @@
       </el-table-column>
 
       <el-table-column
-        label="备注"
+        label="server"
         min-width="300px"
+        prop="server"
       />
 
       <el-table-column
@@ -196,13 +109,95 @@
         </template>
       </el-table-column>
     </el-table>
-    <!-- <pagination
-      v-show="total>0"
-      :total="total"
-      :page.sync="listQuery.page"
-      :limit.sync="listQuery.limit"
-      @pagination="tableData"
-    /> -->
+
+    <el-dialog
+      :title="addFlag?'新增服务器':'修改服务器'"
+      style="text-align:left !important"
+      :visible.sync="dialogVisible"
+      :before-close="handleClose"
+    >
+      <el-form
+        ref="form"
+        label-width="180px"
+      >
+        <el-form-item
+          label="ID"
+          style="width:280px"
+        >
+          <el-input
+            v-model="book.id"
+            placeholder="请输入ID值"
+          />
+        </el-form-item>
+        <el-form-item
+          label="name"
+          style="width:280px"
+        >
+          <el-input
+            v-model="book.name"
+            placeholder="请输入服务器名字"
+          />
+        </el-form-item>
+        <el-form-item
+          label="status"
+          style="width:280px"
+        >
+          <el-input
+            v-model="book.status"
+            placeholder="请输入服务器状态"
+          />
+        </el-form-item>
+        <el-form-item
+          label="server"
+          style="width:280px"
+        >
+          <el-input
+            v-model="book.server"
+            placeholder="请输入分区"
+          />
+        </el-form-item>
+      </el-form>
+      <span
+        slot="footer"
+        class="dialog-footer"
+      >
+        <el-button
+          type="success"
+          @click="saveBook()"
+        >提交</el-button>
+        <el-button
+          type="primary"
+          @click="dialogVisible = false"
+        >取消</el-button>
+      </span>
+    </el-dialog>
+    <el-dialog
+      title="提示"
+      style="text-align:left !important"
+      :visible.sync="dialog2Visible"
+      :before-close="handleClose"
+    >
+      <span>你确定要删除这本图书吗?</span>
+      <span
+        slot="footer"
+        class="dialog-footer"
+      >
+        <el-button @click="handleDel()">提交</el-button>
+        <el-button
+          type="primary"
+          @click="dialog2Visible = false"
+        >取消</el-button>
+      </span>
+    </el-dialog>
+    <el-pagination
+      :current-page="currentPage"
+      :page-sizes="[5, 10, 15, 20]"
+      :page-size="pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="tableData.length"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
   </div>
 </template>
 
@@ -223,98 +218,148 @@ export default class extends Vue {
   // 初始数据.
   tableData = [{
     id: 1,
-    name: '手Q1区 苍天翔龙',
+    server: '手Q1区',
+    name: ' 苍天翔龙',
     status: '爆满',
     val: 356
   }, {
     id: 2,
-    name: '手Q2区 铁血都督'
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满',
+    val: 356
   }, {
     id: 3,
-    name: '手Q3区 野蛮之锤'
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满',
+    val: 356
 
   }, {
     id: 4,
-    name: '手Q4区 正义爆轰'
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满',
+    val: 356
   }, {
     id: 5,
-    name: '手Q4区 正义爆轰'
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满',
+    val: 356
   }, {
     id: 6,
-    name: '手Q4区 正义爆轰'
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满',
+    val: 356
   }, {
     id: 7,
-    name: '手Q4区 正义爆轰'
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满',
+    val: 356
   }, {
     id: 8,
-    name: '手Q4区 正义爆轰'
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满',
+    val: 356
   }, {
     id: 9,
-    name: '手Q4区 正义爆轰'
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满',
+    val: 356
   }, {
     id: 10,
-    name: '手Q4区 正义爆轰'
-  }, {
-    id: 11,
-    name: '手Q4区 正义爆轰'
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满'
   }, {
     id: 12,
-    name: '手Q4区 正义爆轰'
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满'
   }, {
     id: 13,
-    name: '手Q4区 正义爆轰'
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满'
   }, {
     id: 14,
-    name: '手Q4区 正义爆轰'
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满'
   }, {
     id: 15,
-    name: '手Q4区 正义爆轰'
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满'
   }, {
     id: 16,
-    name: '手Q4区 正义爆轰'
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满'
   }, {
     id: 17,
-    name: '手Q4区 正义爆轰'
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满'
   }, {
     id: 18,
-    name: '手Q4区 正义爆轰'
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满'
   }, {
     id: 19,
-    name: '手Q4区 正义爆轰'
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满'
   }, {
     id: 20,
-    name: '手Q4区 正义爆轰'
+    server: '手Q1区',
+    name: ' 苍天翔龙',
+    status: '爆满'
   }]
+  book={}
+  dialogVisible= false
+  dialog2Visible= false
+  addFlag= true
+  currentPage=1
+  pagesize=5
 
-  private total = 0
-  private list: IArticleData[] = []
-  private listLoading = true
-  private listQuery = {
-    page: 1,
-    limit: 20
+  handleSizeChange(size) {
+    this.pagesize = size
+  }
+  handleCurrentChange(currentPage) {
+    this.currentPage = currentPage
   }
 
-  created() {
-    this.getList()
-  }
   handleClick(row) {
     this.$alert(row, 'hello', {
-      confirmButtonText: '确定',
-      callback: action => {
-        this.$message({
-          type: 'info',
-          message: `action: ${action}`
-        })
-      }
+      confirmButtonText: '确定'
+      // callback: action => {
+      // this.$message({
+      // type: 'info',
+      // message: `action: ${action}`
+      // })
+      // }
     })
   }
+  handleEdit(id, row) {
+    this.book = Object.assign({}, row)
+    this.dialogVisible = true
+    this.addFlag = false
+    // _id=id
+  }
   deleteRow(index, rows) {
-    rows.splice(index, 1)
     this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
       confirmButtonText: '确定',
       cancelButtonText: '取消',
       type: 'warning'
     }).then(() => {
+      rows.splice(index, 1)
       this.$message({
         type: 'success',
         message: '删除成功!'
@@ -326,34 +371,24 @@ export default class extends Vue {
       })
     })
   }
-
-  handleEdit(index, row) {
-    this.$prompt('请输入邮箱', '提示', {
-      confirmButtonText: '确定',
-      cancelButtonText: '取消',
-      inputPattern: /[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?/,
-      inputErrorMessage: '邮箱格式不正确'
-    }).then(({ value }) => {
-      this.$message({
-        type: 'success',
-        message: '你的邮箱是: ' + value
+  async saveBook() {
+    try {
+      await this.tableData.push({
+        id: this.book.id,
+        name: this.book.name,
+        status: this.book.status,
+        server: this.book.server
       })
-    }).catch(() => {
+      this.dialogVisible = false
+      this.book = {}
       this.$message({
-        type: 'info',
-        message: '取消输入'
+        message: '添加成功',
+        type: 'success'
       })
-    })
-  }
-  private async getList() {
-    this.listLoading = true
-    const { data } = await getArticles(this.listQuery)
-    this.list = data.items
-    this.total = data.total
-    // Just to simulate the time of the request
-    setTimeout(() => {
-      this.listLoading = false
-    }, 0.5 * 1000)
+    // this.tableData
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
 </script>
