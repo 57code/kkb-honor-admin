5b6fcfda300aefbf5b4a5d3575d701df64179ed0
yt0379 <yt0379@qq.com>
2020-05-18 12:21:16 +0800

    public-path

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ const mockServerPort = 9528 // TODO: get this variable from setting.ts
 const name = 'Vue Typescript Admin' // TODO: get this variable from setting.ts
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/kkb-admin' : '/',
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
   devServer: {
