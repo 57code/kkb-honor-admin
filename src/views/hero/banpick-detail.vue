1117b190aaf8b187f97a7269da4a176c699246aa
yt0379 <yt0379@qq.com>
2020-05-18 12:15:23 +0800

    publicPath

--- src/views/hero/banpick-detail.vue
+++ src/views/hero/banpick-detail.vue
@@ -3,60 +3,73 @@
     <!-- 周期选择 -->
     <div class="range">
       <el-radio-group v-model="range">
-        <el-radio-button label="week">周</el-radio-button>
-        <el-radio-button label="month">月</el-radio-button>
-        <el-radio-button label="quarter">季度</el-radio-button>
-        <el-radio-button label="year">年</el-radio-button>
+        <el-radio-button label="week">
+          周
+        </el-radio-button>
+        <el-radio-button label="month">
+          月
+        </el-radio-button>
+        <el-radio-button label="quarter">
+          季度
+        </el-radio-button>
+        <el-radio-button label="year">
+          年
+        </el-radio-button>
       </el-radio-group>
     </div>
-    <line-chart v-if="detail" height="100%" width="100%" :chart-data="this.chartData" />
-    <p v-else>没有任何数据...</p>
+    <line-chart
+      v-if="detail"
+      height="100%"
+      width="100%"
+      :chart-data="this.chartData"
+    />
+    <p v-else>
+      没有任何数据...
+    </p>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import LineChart from "./components/LineChart.vue";
-import { getBanpickDetail } from "@/api/heros";
-import { BanPickDetailAndHero, RangeType, RangeData } from "@/api/types";
-import { getDateArray } from "@/utils/gen-month-date";
+import { Component, Vue } from 'vue-property-decorator'
+import LineChart from './components/LineChart.vue'
+import { getBanpickDetail } from '@/api/heros'
+import { BanPickDetailAndHero, RangeType, RangeData } from '@/api/types'
+import { getDateArray } from '@/utils/gen-month-date'
 
 const rangeData: RangeData = {
-  week: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"],
+  week: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
   month: getDateArray(),
-  quarter: ["一季度", "二季度", "三季度", "四季度"],
-  year: new Array(12).fill(1).map((v,i)=>(i+1)+'月')
-};
+  quarter: ['一季度', '二季度', '三季度', '四季度'],
+  year: new Array(12).fill(1).map((v, i) => (i + 1) + '月')
+}
 
 @Component({
-  name: "BanPickDetail",
+  name: 'BanPickDetail',
   components: {
     LineChart
   }
 })
 export default class extends Vue {
-  range: RangeType = "week";
+  range: RangeType = 'week';
 
   detail: BanPickDetailAndHero | null = null;
 
   async created() {
-    const res = await getBanpickDetail(this.$route.params.id);
-    this.detail = res.data.detail;
-    
+    const res = await getBanpickDetail(this.$route.params.id)
+    this.detail = res.data.detail
   }
 
-  get chartData() {    
-    
+  get chartData() {
     if (!this.detail) {
-      return { heroName: "", xAxisData: [], banData: [], pickData: [] };
+      return { heroName: '', xAxisData: [], banData: [], pickData: [] }
     }
 
-    const xAxisData = rangeData[this.range];
-    const banData = this.detail[this.range].ban;
-    const pickData = this.detail[this.range].pick;
+    const xAxisData = rangeData[this.range]
+    const banData = this.detail[this.range].ban
+    const pickData = this.detail[this.range].pick
     const data = { heroName: this.detail.hero.name, xAxisData, banData, pickData }
 
-    return data;
+    return data
   }
 }
 </script>
